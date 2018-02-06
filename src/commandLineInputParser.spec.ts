765b5f08d9073fcbdd512d3a19b7cde0f5977bd9
samuelnilsson <samni698@gmail.com>
2018-02-06 22:22:45 +0100

    Fixed failing test. #17.

--- src/commandLineInputParser.spec.ts
+++ src/commandLineInputParser.spec.ts
@@ -111,7 +111,7 @@ describe('commandLineInputParser', () => {
         [ '-q', '--quality' ],
         {
           type: 'int',
-          help: 'The vorbis quality (0-10 [default = 3] for vorbis or 0-9 [default = 4] (lower value is higher quality) for mp3)'
+          help: 'The output quality (0-10 [default = 3] for vorbis or 0-9 [default = 4] (lower value is higher quality) for mp3)'
         }
       ).calledOnce);
     });
